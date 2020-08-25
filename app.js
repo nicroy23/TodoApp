18a637d84892448103c5b7eb37b4f8719cda9fb5
nic_roy23 <nic5.roy23@gmail.com>
2020-08-25 15:36:56 -0400

    Added delete task.

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 let list = document.querySelector("#todo-list");
-let todoInput = document.querySelector("#todo-in")
+let todoInput = document.querySelector("#todo-in");
+let deleteBtn;
 //let todos = ["Faire mes devoirs", "Manger des pommes", "Jouer aux jeux video"];
+
+let todoId = 0;
 
 todoInput.addEventListener("keydown", () => {
     if (event.keyCode === 13) {
-        list.insertAdjacentHTML("beforeend", `<li class="list-item">${todoInput.value}</li>`);
+        list.insertAdjacentHTML("beforeend", `<li class="list-item" id="${todoId}">${todoInput.value}<button class="delete-btn" id="btn${todoId}" onclick="deleteTask(${todoId})">X</button></li>`);
+        
         todoInput.value = '';
+        todoId++;
     }
-})
+});
+
+let deleteTask = function(id) {
+    let toDelete = document.getElementById(id.toString());
+
+    toDelete.outerHTML = '';
+}
